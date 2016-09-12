7ca545525f592637837651b3063acd3cb9b1edaf
Manuel Freiberger <manuel.freiberger@gmx.at>
2016-09-12 21:11:23 +0200

    Added a user config for the unit tests.

---
+++ test/rs11_user_config.hpp
@@ -0,0 +1,16 @@
+#ifndef RS11_USER_CONFIG_HPP
+#define RS11_USER_CONFIG_HPP
+
+// If non-zero, GSL support is added.
+#define RS11_HAVE_GSL   1
+
+// ----=====================================================================----
+//     Private section.
+//     Do not modify the code below.
+// ----=====================================================================----
+
+// The version of the user configuration file. The library can check this
+// version to guarantee the compatibility of the configuration file.
+#define RS11_USER_CONFIG_VERSION   1
+
+#endif // RS11_USER_CONFIG_HPP
