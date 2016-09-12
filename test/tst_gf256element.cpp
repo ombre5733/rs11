2638b6b5ead4058b5aad87334de25b235f2ad9a6
Manuel Freiberger <manuel.freiberger@gmx.at>
2016-09-12 20:08:17 +0200

    Some more test cases.

--- test/tst_gf256element.cpp
+++ test/tst_gf256element.cpp
@@ -4,6 +4,24 @@
 
 using namespace rs11::Galois;
 
+TEST_CASE("addition", "[gf256]")
+{
+    for (unsigned value = 0; value < 256; ++value)
+    {
+        GF256Value a(value);
+        REQUIRE(a + a == GF256Value(0));
+    }
+}
+
+TEST_CASE("subtraction", "[gf256]")
+{
+    for (unsigned value = 0; value < 256; ++value)
+    {
+        GF256Value a(value);
+        REQUIRE(a - a == GF256Value(0));
+    }
+}
+
 TEST_CASE("inversion", "[gf256]")
 {
     for (unsigned value = 1; value < 256; ++value)
